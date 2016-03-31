e5772efe7aa980c996ad1fc9a312878ff66de61a
Dave <davue@bluewin.ch>
2016-03-31 18:04:05 +0200

    added license

--- src/de/luad4j/Main.java
+++ src/de/luad4j/Main.java
@@ -1,3 +1,21 @@
+/*
+ * Lua-Discord4J - Lua wrapper for Discord4J Discord API
+ * Copyright (C) 2016
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package de.luad4j;
 
 import org.luaj.vm2.Globals;
@@ -9,6 +27,7 @@ import sx.blah.discord.api.ClientBuilder;
 import sx.blah.discord.api.EventDispatcher;
 import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.util.DiscordException;
+
 
 public class Main 
 {
@@ -39,7 +58,7 @@ public class Main
 		} 
 		catch (LuaError err)
 		{
-			System.err.println("Error occured while loading lua mine file!");
+			System.err.println("Error occured while loading lua main file!");
 			err.printStackTrace();
 		}
 		
@@ -50,6 +69,6 @@ public class Main
 	
 	private static void registerLuaFunctions()
 	{
-		mLuaEnv.set("sendMessage", new sendMessage(mDiscordClient));
+		mLuaEnv.set("sendMessage", new sendMessage());
 	}
 }
--- src/de/luad4j/luafunc/sendMessage.java
+++ src/de/luad4j/luafunc/sendMessage.java
@@ -1,10 +1,28 @@
+/*
+ * Lua-Discord4J - Lua wrapper for Discord4J Discord API
+ * Copyright (C) 2016
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package de.luad4j.luafunc;
 
 import org.luaj.vm2.LuaValue;
 import org.luaj.vm2.Varargs;
 import org.luaj.vm2.lib.VarArgFunction;
 
-import sx.blah.discord.api.IDiscordClient;
+import de.luad4j.Main;
 import sx.blah.discord.util.DiscordException;
 import sx.blah.discord.util.HTTP429Exception;
 import sx.blah.discord.util.MessageBuilder;
@@ -12,13 +30,6 @@ import sx.blah.discord.util.MissingPermissionsException;
 
 public class sendMessage extends VarArgFunction
 {
-	private IDiscordClient mClient;
-	
-	public sendMessage(IDiscordClient client)
-	{
-		mClient = client;
-	}
-	
 	@Override
 	public Varargs invoke(Varargs v) 
 	{	
@@ -26,7 +37,7 @@ public class sendMessage extends VarArgFunction
 		
 		try 
 		{
-			new MessageBuilder(mClient).withChannel(v.tojstring(1)).withContent(v.tojstring(2)).build();
+			new MessageBuilder(Main.mDiscordClient).withChannel(v.tojstring(1)).withContent(v.tojstring(2)).build();
 			returnval = LuaValue.varargsOf(new LuaValue[] {LuaValue.valueOf(true), NIL});
 		} 
 		catch (HTTP429Exception e)
