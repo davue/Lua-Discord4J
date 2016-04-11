2459b9395b73260229374f08213eedafb519e47d
Dave <davue@bluewin.ch>
2016-04-11 22:02:59 +0200

    added license

--- src/de/luad4j/lua/obj/LuaMessage.java
+++ src/de/luad4j/lua/obj/LuaMessage.java
@@ -16,29 +16,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package de.luad4j.lua.func.audio;
+package de.luad4j.lua.obj;
 
-import org.luaj.vm2.LuaValue;
-import org.luaj.vm2.lib.OneArgFunction;
+public class LuaMessage {
 
-import de.luad4j.Main;
-import sx.blah.discord.util.DiscordException;
-
-public class PauseAudio extends OneArgFunction
-{
-
-	@Override
-	public LuaValue call(LuaValue voicechannelid)
-	{
-		try 
-		{
-			Main.mDiscordClient.getVoiceChannelByID(voicechannelid.tojstring()).getAudioChannel().pause();
-			return LuaValue.NIL;
-		} 
-		catch (DiscordException e) 
-		{
-			e.printStackTrace();
-			return LuaValue.valueOf("DiscordException");
-		}
-	}
 }
