a19376701083f35dfec13fe286529f956f9611b9
Dave <davue@bluewin.ch>
2016-04-19 05:54:43 +0200

    more work on LuaChannel

--- src/de/luad4j/lua/obj/LuaChannel.java
+++ src/de/luad4j/lua/obj/LuaChannel.java
@@ -1,15 +1,20 @@
 package de.luad4j.lua.obj;
 
+import java.util.List;
+
+import org.luaj.vm2.LuaError;
 import org.luaj.vm2.LuaValue;
 import org.luaj.vm2.Varargs;
 import org.luaj.vm2.lib.OneArgFunction;
 import org.luaj.vm2.lib.VarArgFunction;
+import org.luaj.vm2.lib.ZeroArgFunction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import de.luad4j.Main;
 import de.luad4j.events.JavaErrorEvent;
 import sx.blah.discord.handle.obj.IChannel;
+import sx.blah.discord.handle.obj.IInvite;
 import sx.blah.discord.util.DiscordException;
 import sx.blah.discord.util.HTTP429Exception;
 import sx.blah.discord.util.MissingPermissionsException;
@@ -31,6 +36,13 @@ public class LuaChannel
 		mLuaChannel.set("changePosition", new ChangePosition());
 		mLuaChannel.set("changeTopic", new ChangeTopic());
 		mLuaChannel.set("createInvite", new CreateInvite());
+		mLuaChannel.set("delete", new Delete());
+		mLuaChannel.set("getCreationDate", new GetCreationDate());
+		mLuaChannel.set("getGuild", new GetGuild());
+		mLuaChannel.set("getID", new GetID());
+		mLuaChannel.set("getInvites", new GetInvites());
+		mLuaChannel.set("getLastReadMessage", new GetLastReadMessage());
+		mLuaChannel.set("getLastReadMessageID", new GetLastReadMessageID());
 	}
 	
 	private class ChangeName extends OneArgFunction
@@ -106,6 +118,113 @@ public class LuaChannel
 				Main.mDiscordClient.getDispatcher().dispatch(new JavaErrorEvent(e.getClass().getSimpleName() + ":" + e.getMessage()));
 			}*/
 					
+			return LuaValue.NIL;
+		}
+	}
+	
+	private class Delete extends ZeroArgFunction
+	{
+		@Override
+		public LuaValue call()
+		{
+			try
+			{
+				mChannel.delete();
+			}
+			catch (MissingPermissionsException | HTTP429Exception | DiscordException e)
+			{
+				logger.error(e.getMessage());
+				Main.mDiscordClient.getDispatcher().dispatch(new JavaErrorEvent(e.getClass().getSimpleName() + ":" + e.getMessage()));
+			}
+			
+			return LuaValue.NIL;
+		}
+	}
+	
+	private class GetCreationDate extends ZeroArgFunction
+	{
+		@Override
+		public LuaValue call()
+		{
+			return LuaValue.valueOf(mChannel.getCreationDate().toString());
+		}
+	}
+	
+	private class GetGuild extends ZeroArgFunction
+	{
+		@Override
+		public LuaValue call()
+		{
+			return (new LuaGuild(mChannel.getGuild())).getTable();
+		}
+	}
+	
+	private class GetID extends ZeroArgFunction
+	{
+		@Override
+		public LuaValue call()
+		{
+			return LuaValue.valueOf(mChannel.getID());
+		}
+	}
+	
+	private class GetInvites extends ZeroArgFunction
+	{
+		@Override
+		public LuaValue call()
+		{
+			/*try
+			{
+				List<IInvite> invites = mChannel.getInvites();
+				LuaValue luaInvite = LuaValue.tableOf();
+				for(IInvite invite : invites)
+				{
+					LuaValue.set(luaInvite.length(), (new LuaInvite(invite)).getTable());
+				}
+			}
+			catch (LuaError | DiscordException | HTTP429Exception e)
+			{
+				logger.error(e.getMessage());
+				Main.mDiscordClient.getDispatcher().dispatch(new JavaErrorEvent(e.getClass().getSimpleName() + ":" + e.getMessage()));
+			}*/
+			
+			return LuaValue.NIL;
+		}
+	}
+	
+	private class GetLastReadMessage extends ZeroArgFunction
+	{
+		@Override
+		public LuaValue call()
+		{
+			return (new LuaMessage(mChannel.getLastReadMessage())).getTable();
+		}
+	}
+	
+	private class GetLastReadMessageID extends ZeroArgFunction
+	{
+		@Override
+		public LuaValue call()
+		{
+			return LuaValue.valueOf(mChannel.getLastReadMessageID());
+		}
+	}
+	
+	private class GetMessageByID extends OneArgFunction
+	{
+		@Override
+		public LuaValue call(LuaValue messageID)
+		{
+			return (new LuaMessage(mChannel.getMessageByID(messageID.tojstring()))).getTable();
+		}
+	}
+	
+	// TODO: implement MessageList
+	private class GetMessages extends ZeroArgFunction
+	{
+		@Override
+		public LuaValue call()
+		{
 			return LuaValue.NIL;
 		}
 	}
